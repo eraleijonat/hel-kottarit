66508c3618346cbf6b9a0480f54feab27956c42b
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-01-24 11:32:53 +0200

    include build for gh pages

---
+++ build/_app/immutable/chunks/4-51cef464.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/lippukunta/_page.svelte-e9d7954d.js";const e=!0;export{t as component,e as has_server_load};
---
+++ build/_app/immutable/chunks/5-ac22d336.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/yhteystiedot/_page.svelte-e9d7954d.js";const e=!0;export{t as component,e as has_server_load};
