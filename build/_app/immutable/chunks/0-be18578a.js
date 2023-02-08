f804414ff99fb6d30976f8bdb674439d32ee544c
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-02-08 13:23:39 +0200

    removed build

--- build/_app/immutable/chunks/0-be18578a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-aee1e79d.js";const e=!0;export{t as component,e as has_server_load};
--- build/_app/immutable/chunks/3-fed85dd9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/johtajille/_page.svelte-e131a770.js";const e=!0;export{t as component,e as has_server_load};
