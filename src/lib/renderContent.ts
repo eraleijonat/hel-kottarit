e0cde0173e52081a6281afb6623728dd1f862f62
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-02-28 01:55:59 +0200

    fix imports

--- src/lib/renderContent.ts
+++ src/lib/renderContent.ts
@@ -1,6 +1,4 @@
 import type { RegisteredComponent } from '@builder.io/sdk-svelte';
-import InstagramFeed from './instagramFeed.svelte';
-import NavigateButton from './NavigateButton.svelte';
 
 export const BUILDER_PUBLIC_API_KEY = '974369f91b9843f2a04b2475e80baf4a';
 
