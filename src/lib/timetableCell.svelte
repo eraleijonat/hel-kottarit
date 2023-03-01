89b7ec8eb68f3837625c21bb7ea8b89717834192
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-03-01 15:51:05 +0200

    tweaked timetable cell text

--- src/lib/timetableCell.svelte
+++ src/lib/timetableCell.svelte
@@ -30,7 +30,12 @@
 
 <style lang="scss">
 	.cell {
+		display: flex;
+		align-items: center;
+		justify-content: center;
 		padding: 8px;
+		border-radius: 4px;
+		text-align: center;
 	}
 
 	.cellText {
