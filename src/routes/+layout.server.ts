347fc0f8c7bee575063f1cc5b3d56ebcd9603187
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-03-05 22:48:41 +0200

    use prerendering

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -17,4 +17,5 @@ export async function load({ request, params, url }): Promise<{
 	return { content };
 }
 
-// export const prerender = true;
+export const prerender = true;
+export const ssr = true;
