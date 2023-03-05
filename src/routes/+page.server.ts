f421e3a2ed881c62289d81086beed25ae319717e
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-03-05 23:45:03 +0200

    +page.server.ts

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import { BUILDER_PUBLIC_API_KEY } from '$lib/builder/renderContent';
+import { getContent } from '@builder.io/sdk-svelte';
+import type { BuilderContent } from '@builder.io/sdk-svelte/package/types/builder-content';
+
+export async function load({ request, params, url }): Promise<{
+	content: BuilderContent | null;
+}> {
+	// fetch your Builder content
+	const content = await getContent({
+		model: 'page',
+		apiKey: BUILDER_PUBLIC_API_KEY,
+		options: undefined,
+		userAttributes: {
+			urlPath: url.pathname
+		}
+	});
+	return { content };
+}
+
+export const prerender = true;
