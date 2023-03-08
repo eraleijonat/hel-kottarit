6d135e9502d09847b54f9a773a73e6d366762afc
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-03-08 11:37:27 +0200

    tweaked calendar style

--- src/lib/builder/calendar.svelte
+++ src/lib/builder/calendar.svelte
@@ -2,12 +2,13 @@
 	import type { BuilderAttributes } from './types';
 
 	export let attributes: BuilderAttributes;
+	const calendarBackground = 'e3e9ff'; // background color without the #
 </script>
 
-<div {...attributes} class="calendar">
+<div {...attributes} class="calendarWrapper">
 	<iframe
 		title="Helsingin Erä-Leijonat - Kalenteri"
-		src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23015938&ctz=Europe%2FHelsinki&src=dWY2aDVmcW5zYWYyZm5yczZ0cnM0OTA2cmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5"
+		src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23{calendarBackground}&ctz=Europe%2FHelsinki&src=dWY2aDVmcW5zYWYyZm5yczZ0cnM0OTA2cmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5"
 		width="100%"
 		height="600"
 		frameborder="0"
@@ -17,12 +18,14 @@
 
 <style lang="scss">
 	@use '../src/style/_variables.scss';
-	.calendar {
+	.calendarWrapper {
 		width: 100%;
 		height: 100%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		background-color: variables.$main-dark;
+		background-color: #e3e9ff;
+		border-radius: 8px;
+		padding: 16px 12px;
 	}
 </style>
