8418a9a2b98081882436e89944366ff111c13e74
Veikko Jääskeläinen <veikko.jaaskelainen@measurlabs.com>
2023-04-06 15:37:44 +0300

    fixed tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,80 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: [
+		'plugin:import/recommended',
+		'plugin:import/typescript',
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
 		'prettier',
-		'plugin:import/recommended'
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking'
 	],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['@typescript-eslint', 'import'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			},
+			rules: {
+				// https://github.com/ota-meshi/eslint-plugin-svelte/issues/348
+				'@typescript-eslint/no-unused-vars': 'off',
+				// https://github.com/Cweili/svelte-fa/issues/239#issuecomment-1319364670
+				'import/no-named-as-default': 'off'
+			}
+		}
+	],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'import/resolver': {
+			typescript: {
+				project: 'tsconfig.json'
+			}
+		}
+	},
+	rules: {
+		'no-console': 'error',
+		'import/order': [
+			'error',
+			{
+				groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true
+				}
+			}
+		],
+		'no-undef': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				ignoreRestSiblings: true,
+				varsIgnorePattern: '^_',
+				argsIgnorePattern: '^_'
+			}
+		],
+		'import/no-restricted-paths': [
+			'error',
+			{ zones: [{ from: './src/routes/**/*', target: './src/lib/**/*' }] }
+		],
+		'import/no-unresolved': [2, { caseSensitiveStrict: true }]
 	},
 	parserOptions: {
+		tsconfigRootDir: __dirname,
 		sourceType: 'module',
-		ecmaVersion: 2020
+		project: 'tsconfig.json',
+		ecmaVersion: 'latest',
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
-	rules: {
-		'import/no-unresolved': [2, { caseSensitiveStrict: true }]
+	globals: {
+		svelteHTML: true
 	}
 };
